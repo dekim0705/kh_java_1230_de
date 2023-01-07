438630bb2b176c92b14d1e4e6f2dec917d984d5f
dekim0705 <dekim0705@gmail.com>
2023-01-07 22:30:51 +0900

    상근날드 힘들다

--- src/상근날드/SetMenu.java
+++ src/상근날드/SetMenu.java
@@ -8,37 +8,41 @@ import java.util.Scanner;
 // 입력은 총 5개의 값을 연속으로 입력 하는데 햄버거 3가지, 음료 2가지의 가격을 연속으로 입력; 햄버거를 0,1,2에 넣고 음료를 3,4에 넣는것
 // 햄버거 3가지 중 가장 싼 메뉴 선택, 음료 2중 싼 것 선택하고, 세트 메뉴이기 때문에 50원 할인
 // 총 세트 메뉴의 금액 구하는 문제
+
 public class SetMenu {
     public static void main(String[] args) {
-        System.out.println("안녕하세요 넘 어려워용");
         Scanner sc = new Scanner(System.in);
         System.out.print("햄버거 3가지, 음료 2가지의 가격을 입력하세요: ");
-        int[] menu = new int[5];
-
-        int minH;
-        int total;
-        for(int i = 0; i < menu[2]; i++) {
-            menu[i] = sc.nextInt();
-            //if (minH > menu[i]) minH = menu[i];
-            total =+ menu[i];
-
-            System.out.println("가격의 총 합은" + total + "입니다");
+        int[] price = new int[5];
+        for (int i = 0; i < price.length; i++) {
+            price[i] = sc.nextInt();
         }
 
+        int minH = price[0];
+        int minD = price[3];
+
+        for (int i = 0; i <= price[3]; i++) ; // 조건식이 i < price[3]일때랑 어떻게 다르지?
+        if (minH < price[1] && minH < price[2]) {
+        } else if (price[1] > price[2]) {
+            minH = price[2];
+        } else {
+            minH = price[1];
+        }      System.out.println("가장 저렴한 햄버거는 " + minH + "원 입니다");
 
 
+        for (int i = 0; i <= price[4]; i++);
+        if (minD < price[4]) {
+            System.out.println("가장 저렴한 음료는 " + minD + "원 입니다");
+        } else {
+            minD = price[4];
+                    System.out.println("가장 저렴한 음료는 " + minD + "원 입니다.");
+        }
 
-//        for(int i = 0; i < menu[2]; i++) {
-//            if (minH > menu[i]) minH = menu[i];
-//        }
-//        for(int i = 0; i >= menu[4]; i++) {
-//            if(minD > menu[i]) minD = menu[i];
-//        }
-
-       // System.out.println("가장 싼 햄버거는 " + minH + "원 입니다.");
-        //System.out.println("가장 싼 음료는 " + minD + "원 입니다.");
-
+        System.out.println("가장 저렴한 세트는 " + (minH + minD-50) + "원 입니다." );
 
     }
 
 }
+
+
+
