b97435ef9c9c9b39cb4048ed36ba7fa40c07e7e4
dekim0705 <dekim0705@gamil.com>
2023-01-08 18:02:59 +0900

    first commit after clone

--- src/상근날드/SetMenu.java
+++ src/상근날드/SetMenu.java
@@ -21,22 +21,23 @@ public class SetMenu {
         int minH = price[0];
         int minD = price[3];
 
-        for (int i = 0; i <= price[3]; i++) ; // 조건식이 i < price[3]일때랑 어떻게 다르지?
+        //for (int i = 0; i <= price[3]; i++) // 조건식이 i < price[3]일때랑 어떻게 다르지?
         if (minH < price[1] && minH < price[2]) {
         } else if (price[1] > price[2]) {
             minH = price[2];
         } else {
             minH = price[1];
-        }      System.out.println("가장 저렴한 햄버거는 " + minH + "원 입니다");
+        }      //System.out.println("가장 저렴한 햄버거는 " + minH + "원 입니다");
 
 
-        for (int i = 0; i <= price[4]; i++);
+        //for (int i = 0; i <= price[4]; i++)
         if (minD < price[4]) {
-            System.out.println("가장 저렴한 음료는 " + minD + "원 입니다");
+
         } else {
             minD = price[4];
-                    System.out.println("가장 저렴한 음료는 " + minD + "원 입니다.");
         }
+                    //System.out.println("가장 저렴한 음료는 " + minD + "원 입니다.");
+
 
         System.out.println("가장 저렴한 세트는 " + (minH + minD-50) + "원 입니다." );
 
